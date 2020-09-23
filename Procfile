a82be02fb0b003ab8f865a2d84069ebbb1c4a466
V S Sreejeet <vsreejeet@gmail.com>
2020-09-23 20:06:57 +0530

    added procfile for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn userform.wsgi:application
--- userform/settings.py
+++ userform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,3 +148,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'test')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'test')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+# Heroku config
+django_heroku.settings(locals())
